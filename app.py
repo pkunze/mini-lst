0a05d8563bc80a82edbe53a0f992504a59d765fa
Paul Kunze <paul.kunze@materna.group>
2024-05-24 10:33:09 +0200

    add gong and text to speech

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import os
+import azure.cognitiveservices.speech as speechsdk
 import streamlit as st
+from preferredsoundplayer import *
 
 st.set_page_config(page_title="BF-Tag Notfallabfrage", page_icon="🚒", initial_sidebar_state="collapsed")
 
 st.header("Wo ist der Notfallort?")
-st.text_input("Stadt", value="Dresden")
+city = st.text_input("Stadt", value="Dresden")
 address = st.text_input("Adresse", placeholder="Lilienstraße 1 / Bismarksäule")
 
 if(not address):
@@ -12,7 +15,8 @@ if(not address):
 st.header("Was ist passiert?")
 
 description = st.text_area("Kurzbeschreibung", placeholder="Was ist passiert?", help="Stichpunktartig den Unfall/Notfall-Hergang beschreiben.")
-if(not description):
+category = st.selectbox("Einsatz-Kategorie", ["Brandmeldeanlage", "Brandeinsatz", "Technische Hilfeleistung", "Medizinischer Notfall", "Tierrettung"])
+if(not description or not category):
     st.stop()
 
 st.header("Wer ruft an?")
@@ -50,4 +54,37 @@ send_mtf = col_mtf.checkbox("MTF", value=False)
 st.markdown("---")
 
 if(send_hlf or send_mtf):
-    st.button("Alarmieren", use_container_width=True, type="primary")
+    alarm = st.button("Alarmieren", use_container_width=True, type="primary")
+    
+    if(alarm):
+        speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
+        audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+        speech_config.speech_synthesis_voice_name='de-DE-KatjaNeural'
+        speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+
+        soundplay("./firehouse_alarm_gong.mp3")
+        sleep(5)
+        
+        alarm_text = f"Alarm! {category}! Es kommt zum Einsatz: "
+        if(send_hlf and send_mtf):
+            alarm_text += "HLF-10 und MTF."
+        elif(send_hlf):
+            alarm_text += "HLF-10."
+        elif(send_mtf):
+            alarm_text += "MTF."
+        
+        if(city):
+            alarm_text += f" {city}."
+        
+        if(address):
+            alarm_text += f" {address} -"
+
+        if(description):
+            alarm_text += f" {description}."
+            
+        if(patient_count == 1):
+            alarm_text += f" Eine Person verletzt."
+        elif(patient_count > 1):
+            alarm_text += f" {patient_count} Personen verletzt."
+        
+        speech_synthesis_result = speech_synthesizer.speak_text_async(alarm_text).get()
